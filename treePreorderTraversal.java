ebb4e6e0f4e5f8222cd935404e5e3dc230b863c0
XayLei <xlgyyz@163.com>
2017-04-01 17:08:22 +0800

    Create treePreorderTraversal.java

---
+++ treePreorderTraversal.java
@@ -0,0 +1,41 @@
+//定义树的节点类
+class TreeNode{
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x){
+         val = x;
+     }
+}
+
+//use recursive method to preorder traversal
+List<Integer> preorderTraversal(TreeNode root){
+     List<Integer> result = new ArrayList<>();
+     if(root == null) return result;
+     
+     result.add(root);
+     result.addAll(preorderTraversal(root.left));
+     result.addAll(preorderTraversal(root.right));
+     reutrn result;
+}
+
+
+//use Non-recursive
+List<Integer> preorderTraversal(TreeNode root){
+     List<Integer> result = new ArrayList<>();
+     if(root == null) return result;
+     
+     Stack<TreeNode> stack = new Stack<>();
+     stack.push(root);
+     while(!stack.isEmpty()){
+         TreeNode node = stack.pop();
+         result.add(node);
+         if(node.right != null){
+              stack.push(node.right);
+         }
+         if(node.left != null){
+              stack.push(node.left);
+         }
+     }
+     return result;
+}
