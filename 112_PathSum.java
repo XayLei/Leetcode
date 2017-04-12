e8717224b686dfe6b7b3960cbc97ebb6a97804b8
XayLei <xlgyyz@163.com>
2017-04-12 15:29:35 +0800

    Update 112_PathSum.java

--- 112_PathSum.java
+++ 112_PathSum.java
@@ -1,4 +1,3 @@
-//Recursive Method
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -8,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//Recursive Method
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root == null) return false;
@@ -15,5 +15,33 @@ public class Solution {
         if(root.left == null && root.right == null && root.val==sum) return true;
         
         return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+    }
+}
+
+//Non-recursive
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        Stack<Integer> sums = new Stack<>();
+        Stack<TreeNode> tree = new Stack<>();
+        
+        sums.push(sum);
+        tree.push(root);
+        while(!tree.isEmpty() && root != null){
+            TreeNode top = tree.pop();
+            int value = sums.pop();
+            
+            if(top.left==null && top.right==null && top.val==value) return true;
+            
+            if(top.right != null){
+                tree.push(top.right);
+                sums.push(value-top.val);
+            }
+            
+            if(top.left != null){
+                tree.push(top.left);
+                sums.push(value-top.val);
+            }
+        }
+        return false;
     }
 }
